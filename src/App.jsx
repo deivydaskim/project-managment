9e44fbc045770fb5e34f0272d4dcd1f6733f90a8
deivydas.kim <deivydas.kim@gmail.com>
2024-06-17 13:57:27 +0300

    create switching from noProjectSelected to NewProject

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,31 @@ import ProjectsSidebard from './components/ProjectsSidebar';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
-    selectedProject: undefined,
+    selectedProjectId: undefined,
     projects: [],
   });
 
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null, // Null will be state while project is adding
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebard />
-      <NoProjectSelected />
+      <ProjectsSidebard onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
