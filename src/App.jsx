157953a065f97b15449b02c572061bc4c7a21ce8
deivydas.kim <deivydas.kim@gmail.com>
2024-06-17 13:46:21 +0300

    create page when no projects selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import NewProject from "./components/NewProject";
-import ProjectsSidebard from "./components/ProjectsSidebar";
+import { useState } from 'react';
+
+import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectsSidebard from './components/ProjectsSidebar';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: [],
+  });
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebard />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
