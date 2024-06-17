543a81640d8fecc48c35aebe0019c3d821007f0e
deivydas.kim <deivydas.kim@gmail.com>
2024-06-17 13:22:28 +0300

    create sidebar and skeleton for adding new projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import NewProject from "./components/NewProject";
+import ProjectsSidebard from "./components/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold"></h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebard />
+      <NewProject />
+    </main>
   );
 }
 
